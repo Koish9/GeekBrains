665b5385f52137d42808934f67e8038f86080439
Koishe <danilkotov15@gmail.com>
2023-07-24 08:54:52 +0300

    Домашняя работа за 8 урок

---
+++ Seminar9/SeminarTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿// НАпишите программу которая будет принимать на вход два числа и взвращать сумму его цифр
+int Request(string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int RecursSum(int number)
+{
+    int sum = 0;
+    if ((number/10) == 0) return number;
+    sum = RecursSum(number/10) + number%10;
+
+    return sum;
+}
+
+int number = Request("число");
+Console.WriteLine(RecursSum(number));
---
+++ Seminar9/Seminartask2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая на вход принимает два числа а и в и возводит а в целую степень в с помощью рекурсии
+
+
+int Request(string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int Pow(int A, int B)
+{
+    if(B == 1) return A;
+    int C = Pow(A,B-1)*A;
+    return C;
+}
+
+int numberA = Request("Число A: ");
+int numberB = Request("число B: ");
+
+Pow(numberA,numberB);
+Console.WriteLine(Pow(numberA,numberB));
